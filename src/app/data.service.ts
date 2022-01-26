51ca5b2fdc8658b5be5509b784028be08cf5cbd7
Sayed Hussaini <seh31628@uga.edu>
2022-01-26 12:22:31 -0500

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
   @Input() ticker:any;
   private REST_API_SERVER = "http://api.marketstack.com/v1/";
-  private accessKey = 'a8be7182526274d4ad6d90e425581291'
+  private accessKey = ''
   private stringFirst='http://api.marketstack.com/v1/eod?access_key=a8be7182526274d4ad6d90e425581291&symbols='
   private stringLast = '&date_from=2021-01-01& date_to=2021-02-01';
   constructor(private httpClient: HttpClient) { }
